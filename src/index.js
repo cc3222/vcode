852e3e5818d5622aa58ed569e22946967e3f29b3
jp3222 <juanpc.559@gmail.com>
2022-07-21 20:46:46 -0500

    Servidor funcionando

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-/*
-Para este proyecto se requieren las dependencias
-express, ejs, parser-body, nodemon
+const app = require('./app')
 
-el proyecto se corre con el comando "npm run dev"
-el proyecto no requiere reiniciar el servidor este se reinicia solo al gaurdar los cambios hechos
-*/
-
-const express = require("express")
-const path = require('path')
-const router = require('./rutas/index')
-const bodyParser = require('body-parser')
-app = express()
-
-//settings
-app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname,'vistas'))
-app.set('view engine','ejs')
-//middlewares
-app.use((req,res,next) => {
-    console.log(`${req.url} -${req.mehod}`)
-    next()
-})
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended:false
-}))
-app.use(router)
-//
-app.use(express.static(path.join(__dirname,'public')))
-
-app.listen(app.get('port'),() =>{
-    console.log("servidor iniciado", app.get('port'))
-})
-
+async function main(){
+    app = await app()
+}
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router();
+
+
 router.get('/',(req,res)=>{
     res.render('index',{
         titulo:"VCode"
     })
+})
+
+router.get('/ejem',(req,res)=>{
+    res.render('ejem',{
+        titulo:"ejemplo"
+    })
+})
+
+router.post('/new-contact',(req,res) =>{
+    console.log(req.body);
+    res.send('recibido');
 })
 
 module.exports = router;
